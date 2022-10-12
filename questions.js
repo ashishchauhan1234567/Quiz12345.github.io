df2eff45d2add2f9ab1883b539d4b21c92c2f6c2
ASHISH CHAUHAN <90232448+ashishchauhan1234567@users.noreply.github.com>
2022-10-12 21:07:11 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,311 @@
+
+
+let questions = [
+  {
+  numb: 1,
+  question: "HTML stands for -",
+  answer: "HyperText Markup Language",
+  options: [
+    "HyperText and links Markup Language",
+    "HyperText Markup Language",
+    "HyperText and links Markup Language",
+    "None of these"
+  ]
+},
+{
+  numb: 2,
+  question: " The tags in HTML are -",
+  answer: "not case sensitive",
+  options: [
+    "case-sensitive",
+    "in upper case",
+    "not case sensitive",
+    "in lowercase"
+  ]
+},
+{
+  numb: 3,
+  question: "CSS stands for -",
+  answer: "Cascading style sheets",
+  options: [
+    "Cascade style sheets",
+    "Color and style sheets",
+    "Cascading style sheets",
+    "None of the above"
+  ]
+},
+{
+  numb: 4,
+  question: "Which of the following is correct about JavaScript?",
+  answer: "JavaScript is an Object-Based language",
+  options: [
+    "JavaScript is an Object-Based language",
+    "JavaScript is Assembly-language",
+    "  JavaScript is an Object-Oriented language",
+    " JavaScript is a High-level language"
+  ]
+},
+{
+  numb: 5,
+  question: "Which of the following is not javascript data types?",
+  answer: "All of the mentioned",
+  options: [
+    "Null type",
+    "Undefined type",
+    "Number type",
+    "All of the mentioned"
+  ]
+},
+{
+  numb: 6,
+  question: "What Is Angular JS?",
+  answer: "AngularJS is an open-source front-end web framework",
+  options: [
+    "AngularJS is a closed-source front-end web framework",
+    "AngularJS is an open-source front-end web framework",
+    "AngularJS is an open-source backend web framework",
+    "AngularJS is a closed-source back-end web framework"
+  ]
+},
+{
+  numb: 7,
+  question: " What is React.js?",
+  answer: "Free and open-source JavaScript front-end library",
+  options: [
+    "Open-source JavaScript back-end library",
+    "JavaScript front-end library to create a database",
+    "Free and open-source JavaScript front-end library",
+    " None of the mentioned"
+  ]
+},
+{
+  numb: 8,
+  question: "React.js is written in which of the following language?",
+  answer: "JavaScript",
+  options: [
+    "JavaScript",
+    "java",
+    "C",
+    "C++"
+  ]
+},
+{
+  numb: 9,
+  question: " Which of the following command is used to Install create-react-app?",
+  answer: "npm install -g create-react-app",
+  options: [
+    "npm install create-react-app",
+    "npm install -f create-react-app",
+    "npm install -g create-react-app",
+    "install -g create-react-app"
+  ]
+},
+{
+  numb: 10,
+  question: "The CSS property used to make the rounded borders, or rounded corners around an element is -",
+  answer: "border-radius",
+  options: [
+    "border-collapse",
+    "border-radius",
+    "border-spacing",
+    "None of the above"
+  ]
+},
+
+{
+  numb: 11,
+  question: " The CSS property used to set the distance between the borders of the adjacent cells in the table is -",
+  answer: "border-spacing",
+  options: [
+    "border-collapse",
+    "border-radius",
+    "border-spacing",
+    "None of the above"
+  ]
+},
+
+{
+  numb: 12,
+  question: " Which of the following extension is used to save an HTML file?",
+  answer: ".html",
+  options: [
+    ".hl",
+    ".h",
+    ".htnl",
+    ".html"
+  ]
+},
+
+
+
+
+
+
+
+
+
+
+{
+  numb:13,
+  question: "Who is the father of Computers?",
+  
+  answer: "Charles Babbage",
+  options: [
+    "Charles Babbage",
+    "Dennis Ritchie",
+    "Bjarne Stroustrup",
+    "James Gosling"
+  ]
+},
+{
+  numb: 14,
+  question: " What is the full form of CPU?",
+  answer: "Central Processing Unit",
+  options: [
+    "Computer Processing Unit",
+    "Computer Principle Unit",
+    "Control Processing Unit",
+    "Central Processing Unit"
+  ]
+},
+{
+  numb: 15,
+  question: "Which of the following language does the computer understand?",
+  answer: " Computer understands only Binary Language",
+  options: [
+    "Computer understands only Assembly Language",
+    " Computer understands only Binary Language",
+    "Computer understands only C Language",
+    " Computer understands only BASIC"
+  ]
+},
+{
+  numb: 16,
+  question: "Which of the following is not a characteristic of a computer?",
+  answer: " I.Q.",
+  options: [
+    "Versatility",
+    "Accuracy",
+    " Diligence",
+    " I.Q."
+  ]
+},
+{
+  numb: 17,
+  question: "Which of the following is the smallest unit of data in a computer?",
+  answer: "Bit",
+  options: [
+    "Bit",
+    "KB",
+    "Nibble",
+    "Byte"
+  ]
+},
+{
+  numb: 18,
+  question: "Which of the following is designed to control the operations of a computer?",
+  answer: "System Software",
+  options: [
+    "System Software",
+    "Utility Software",
+    "Application Software",
+    "User"
+  ]
+},
+{
+  numb: 19,
+  question: " Which of the following are physical devices of a computer?",
+  answer: "Hardware",
+  options: [
+    "Software",
+    "Package",
+    "Hardware",
+    " System Software"
+  ]
+},
+{
+  numb: 20,
+  question: "Which of the following is known as the language made up of binary-coded instructions?",
+  answer: "Machine",
+  options: [
+    "High level",
+    " BASIC",
+    "C",
+    "Machine"
+  ]
+},
+{
+  numb: 21,
+  question: "Google (www.google.com) is a:",
+  answer: "Search Engine",
+  options: [
+    "Search Engine",
+    "Number in Math",
+    " Directory of images",
+    " Chat service on the web"
+  ]
+},
+{
+  numb: 22,
+  question: "Which is not an Internet protocol?",
+  answer: "STP",
+  options: [
+    "HTTP",
+  "FTP",
+    "IP",
+    "STP"
+  ]
+},
+
+{
+  numb: 23,
+  question: "Another name for a computer chip is:",
+  answer: "Micro chip",
+  options: [
+    "Execute",
+    "Micro chip",
+    "Microprocessor",
+    "Select"
+  ]
+},
+
+{
+  numb: 24,
+  question:"www stands for:",
+  answer: "World Wide Web",
+  options: [
+    "World Wide Web",
+    "World Wide Wares",
+    "World Wide Wait",
+    "World Wide War"
+  ]
+},
+{
+  numb: 25,
+  question: "WWhat is the full of LAN?",
+  answer: "Local Area Network",
+  options: [
+    "Low-level Access Network",
+    "Local Area Network",
+    "Load Adjusting Network",
+    "Lower Area Network"
+  ]
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
